e90da67abb9ccebbe4eb8d361dd532759e697e69
appolinair2355 <felicitationsbien990@gmail.com>
2025-11-08 10:06:47 +0100

    Add licence management functions in licences.py

---
+++ licences.py
@@ -0,0 +1,42 @@
+import time
+import json
+import os
+
+LICENCES_FILE = "licences.json"
+
+def load_licences():
+    if not os.path.exists(LICENCES_FILE):
+        return {}
+    with open(LICENCES_FILE, "r") as f:
+        return json.load(f)
+
+def save_licences(data):
+    with open(LICENCES_FILE, "w") as f:
+        json.dump(data, f)
+
+def ajouter_licence(licence, hours):
+    data = load_licences()
+    data[licence] = {
+        "hours": hours,
+        "used": False,
+        "user_id": None,
+        "created_at": time.time()
+    }
+    save_licences(data)
+
+def check_licence(licence):
+    data = load_licences()
+    return licence in data and not data[licence]["used"]
+
+def save_licence_usage(licence, user_id):
+    data = load_licences()
+    if licence in data and not data[licence]["used"]:
+        data[licence]["used"] = True
+        data[licence]["user_id"] = user_id
+        data[licence]["used_at"] = time.time()
+        save_licences(data)
+
+def licence_deja_utilisee(licence):
+    data = load_licences()
+    return data.get(licence, {}).get("used", False)
+  
