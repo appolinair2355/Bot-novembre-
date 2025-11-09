1416a80b3515d84e251ac2c7bc45523303c83935
appolinair2355 <felicitationsbien990@gmail.com>
2025-11-09 02:20:05 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,9 @@ import os
 
 class Config:
     def __init__(self):
+        # BOT_TOKEN est nécessaire pour le Polling
         self.BOT_TOKEN   = os.getenv("BOT_TOKEN")
+        # Les variables WEBHOOK et PORT ne sont plus utilisées en Polling
         self.WEBHOOK_URL = os.getenv("WEBHOOK_URL", "").rstrip("/")
         self.PORT        = int(os.getenv("PORT", "10000"))
         if not self.BOT_TOKEN:
@@ -10,4 +12,4 @@ class Config:
     @property
     def webhook_path(self) -> str:
         return f"{self.WEBHOOK_URL}/webhook" if self.WEBHOOK_URL else ""
-      
+        
